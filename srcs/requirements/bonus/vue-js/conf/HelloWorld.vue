a6db1547d23a49f43e7f943ac0c17bd6ccce3d72
Mourdalov Mouslim <mmourdal@inception.42paris.fr>
2023-10-19 20:26:26 +0200

    Modify Makefile, vuejs, permissions, add kuma bonus

---
+++ srcs/requirements/bonus/vue-js/conf/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello-world">
+    <div class="welcome-message">
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Bienvenue sur le site statique en VueJS de Mouslim Mourdalov !",
+    };
+  },
+  mounted() {
+    this.addAnimation();
+  },
+  methods: {
+    addAnimation() {
+      this.$el.classList.add("fade-in");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.hello-world {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.welcome-message {
+  color: #0073e6;
+  font-size: 24px;
+  text-align: center;
+}
+
+.fade-in {
+  opacity: 0;
+  animation: fadeIn 1s ease-in forwards;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
--- srcs/requirements/bonus/vue-js/tools/init.sh
+++ srcs/requirements/bonus/vue-js/tools/init.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-echo "Y" | vue create . --default;
-npm run serve -- --port 4848;
+if ! netstat -tuln | grep ":4848 " > /dev/null
+then
+  echo "Y" | vue create . --default;
+  rm /var/www/static-site/src/components/HelloWorld.vue
+  cp /var/HelloWorld.vue /var/www/static-site/src/components
+  npm run serve -- --port 4848;
+else
+  echo "Un serveur npm est déjà en cours d'exécution sur le port 4848. Aucune action nécessaire."
+fi
